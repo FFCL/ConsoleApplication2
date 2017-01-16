aaebdffaa37f889f5bf70630876cb454111fad80
Administrator <Administrator@LOLICON>
2017-01-16 18:02:43 +0800

    C#change

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;//引入命名空间
 
-namespace ConsoleApplication2
+namespace ConsoleApplication2//命名空间
 {
-    class Program
+    class Program//类
     {
-        static void Main(string[] args)
+        //Main方法是程序运行的入口，也是出口
+        static void Main(string[] args)//名为Main的方法
         {
+            Console.WriteLine("Hello World");//向控制台输出信息
+            Console.ReadKey();//请按任意键以继续
         }
     }
 }
