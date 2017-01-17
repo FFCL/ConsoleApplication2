de983dbb000a220efca87f87151b2229a2417aa5
Administrator <Administrator@LOLICON>
2017-01-17 10:22:55 +0800

    2017-01-17

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //变量声明
+            int a = 0;//int数值类型可以按F12查询最大值和最小值
+            float b = a;//划词，按F1打开msdn查询含义
+            int c = 12;//声明一个变量，并赋值。（在内存里分配一块空间类型为int的空间，然后把数据存储进去）
+            //变量赋值
+            int d = c;//把c的内存空间里的数据复制到d里面去
+            //字符串
+            string s1 = "caonima";
+            string s2 = "buhuibaidua";
+            Console.WriteLine(s1 + s2 + d);//字符串只会拼接，不会进行运算
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "name";
+            int age = 21;
+            char sex = '男';
+            Console.WriteLine("Your name is "+name);
+            Console.WriteLine("Your age is "+age);
+            Console.WriteLine("Your sex is "+sex);
+        }
+    }
+}
